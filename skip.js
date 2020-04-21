458e20e4a83b26abe5de36a50f733922802c7090
GHSam <samclarke@outlook.com>
2020-04-22 00:08:14 +0100

    Refactor code a bit

--- skip.js
+++ skip.js
@@ -1,6 +1,16 @@
+// Only apply URLs with /lecture/ in them
 const lectureUrlRegex = /^https:\/\/www\.coursera\.org\/learn\/uol-[^\/]+\/lecture\//;
+
+// Create live collection of video containers to check.
+// Could use querySelector but getElementsByClassName is
+// much faster and this has to be checked in every DOM
+// mutation so needs to be fast
+const containers = document.getElementsByClassName(
+  "video-main-player-container"
+);
+
 let skipSeconds = 9;
-let enabled = true;
+let isEnabled = false;
 
 /**
  * Sets the extensions toolbar icon
@@ -18,7 +28,7 @@ function setIcon(value) {
 /**
  * Video event handler to skip to 9 seconds if current time is less
  */
-function skipHandler() {
+function skipIntroHandler() {
   if (this.currentTime < skipSeconds) {
     setIcon("icon-active");
     this.currentTime = skipSeconds;
@@ -29,32 +39,25 @@ function skipHandler() {
  * Finds the video element and skips the first 9 seconds
  */
 function skipIntro() {
-  // This is called for every DOM mutation so needs to be fast.
-  // Could use querySelector but getElementsByClassName is
-  // much faster
-  const container = document.getElementsByClassName(
-    "video-main-player-container"
-  )[0];
-
   // Only run if has video and is on a lecture URL
-  if (!container || !lectureUrlRegex.test(window.location)) {
+  if (!containers[0] || !lectureUrlRegex.test(window.location)) {
     setIcon("icon-inactive");
     return;
   }
 
-  const video = container.getElementsByTagName("video")[0];
+  const video = containers[0].getElementsByTagName("video")[0];
   if (video && video.dataset.intro != "skipped") {
     video.dataset.intro = "skipped";
 
     // Add events or call the handler if events have already been called
     if (video.readyState < 3) {
-      // loadedmetadata seems to be the corrent event to use but doesn't
+      // loadedmetadata seems to be the current event to use but doesn't
       // always work, might be Coursera adjusting it sometimes?
       // Using canplay seems to fix the issue.
-      video.addEventListener("loadedmetadata", skipHandler, { once: true });
-      video.addEventListener("canplay", skipHandler, { once: true });
+      video.addEventListener("loadedmetadata", skipIntroHandler, { once: true });
+      video.addEventListener("canplay", skipIntroHandler, { once: true });
     } else {
-      skipHandler.call(video);
+      skipIntroHandler.call(video);
     }
   }
 }
@@ -62,32 +65,32 @@ function skipIntro() {
 // Detect react mutations and fix video if needed
 const observer = new MutationObserver(skipIntro);
 
-function enable() {
-  setIcon("icon-inactive");
-  skipIntro();
+function setEnable(value) {
+  if (isEnabled == value) {
+    return;
+  }
 
-  observer.observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  });
+  isEnabled = value;
+
+  if (value) {
+    setIcon("icon-inactive");
+    skipIntro();
+
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  } else {
+    setIcon("icon-disabled");
+    observer.disconnect();
+  }
 }
 
-function disable() {
-  setIcon("icon-disabled");
-  observer.disconnect();
-}
-
-// Update based on changes from options
+// Update settings based on changes from options
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area === "sync") {
-    if (changes.enabled && enabled !== changes.enabled.newValue) {
-      enabled = changes.enabled.newValue;
-
-      if (enabled) {
-        enable();
-      } else {
-        disable();
-      }
+    if (changes.enabled) {
+      setEnable(changes.enabled.newValue);
     }
 
     if (changes.skipSeconds) {
@@ -104,12 +107,6 @@ chrome.storage.sync.get(
   },
   (data) => {
     skipSeconds = data.skipSeconds;
-    enabled = data.enabled;
-
-    if (enabled) {
-      enable();
-    } else {
-      setIcon("icon-disabled");
-    }
+    setEnable(data.enabled);
   }
 );
