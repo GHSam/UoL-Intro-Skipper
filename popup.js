629351bd0bd1fc909455a78e111ede13384937f8
GHSam <samclarke@outlook.com>
2020-04-22 12:46:30 +0100

    Tweak prettier formatting

--- popup.js
+++ popup.js
@@ -1,39 +1,23 @@
 const timeInput = document.getElementById("time");
 const enabledInput = document.getElementById("enabled");
 
-chrome.storage.sync.get(
-  {
-    skipSeconds: 9,
-    enabled: true,
-  },
-  (data) => {
-    timeInput.value = data.skipSeconds;
-    enabledInput.checked = data.enabled;
-  }
-);
+chrome.storage.sync.get({ skipSeconds: 9, enabled: true }, (data) => {
+  timeInput.value = data.skipSeconds;
+  enabledInput.checked = data.enabled;
+});
 
 timeInput.addEventListener("input", function () {
   const seconds = parseInt(this.value, 10);
   if (!isNaN(seconds)) {
-    chrome.storage.sync.set(
-      {
-        skipSeconds: seconds,
-      },
-      () => {
-        if (seconds != timeInput.value) {
-          timeInput.value = seconds;
-        }
+    chrome.storage.sync.set({ skipSeconds: seconds }, () => {
+      if (seconds != timeInput.value) {
+        timeInput.value = seconds;
       }
-    );
+    });
   } else {
-    chrome.storage.sync.get(
-      {
-        skipSeconds: 9,
-      },
-      (data) => {
-        timeInput.value = data.skipSeconds;
-      }
-    );
+    chrome.storage.sync.get({ skipSeconds: 9 }, (data) => {
+      timeInput.value = data.skipSeconds;
+    });
   }
 });
 
